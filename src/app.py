70202927e9d6068a617a98435a3a950d42dc4bfb
“Cassio-hue” <cassiocvtb2003@gmail.com>
2024-08-08 15:05:11 -0300

    feat: tela login admin

--- src/app.py
+++ src/app.py
@@ -4,12 +4,12 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 
 @app.route("/")
 def hello_world():
-    return "<h1>Hello, World!</h1>"
+  return "<h1>Hello, World!</h1>"
 
 @app.route("/admin", methods=['GET', 'POST'])
 def admin():
-    return render_template('admin/login.html')
+  return render_template('admin/login.html')
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+  app.run(host="0.0.0.0", port=5000, debug=True)
