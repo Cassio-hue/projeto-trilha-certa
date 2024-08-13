811fb3f3b37a49ce2d6f9a6f50577a4f412618d2
Pedro Farias <pfarias29@gmail.com>
2024-08-13 15:01:54 -0300

    fix: arruma o bug do acesso ao documento em diferentes SO's

--- src/db/db.py
+++ src/db/db.py
@@ -4,14 +4,10 @@ class Service():
   def __init__(self):
     self.admin = 'admin.txt'
     self.estudantes = 'estudantes.txt'
-    self.path = os.getcwd() + '/src/db/'
-
+    self.path = os.path.join(os.getcwd(), 'src', 'db')
 
   def logar(self, usr_login, usr_senha):
-    arquivo = open(self.path + self.admin, "r")
-    print()
-    print(self.path)
-    print()
+    arquivo = open(os.path.join(self.path, self.admin), "r")
     data = arquivo.read()
     arquivo.close()
 
@@ -21,15 +17,13 @@ class Service():
 
 
   def criar_aluno(self, cpf):
-    arquivo = open(self.path + self.estudantes, "a")
+    arquivo = open(os.path.join(self.path, self.estudantes), "a")
     arquivo.write(cpf + '\n')
     arquivo.close()
 
   def login_aluno(self, cpf):
-    arquivo = open(self.path + self.estudantes, "r")
-    print()
     print(self.path)
-    print()
+    arquivo = open(os.path.join(self.path, self.estudantes), "r")
     data = arquivo.read()
     arquivo.close()
 
