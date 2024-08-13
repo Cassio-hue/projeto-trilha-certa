4fb32218d1042542e0449f25335389026a9b24b8
Marcolino5 <102527399+Marcolino5@users.noreply.github.com>
2024-08-13 11:55:27 -0300

    :sparkles: feat: tela login usuario e tela menu admin

--- src/app.py
+++ src/app.py
@@ -2,13 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
-@app.route("/")
-def hello_world():
-  return "<h1>Hello, World!</h1>"
+@app.route("/", methods=['GET', 'POST'])
+def aluno_login():
+  return render_template('aluno/login.html')
 
 @app.route("/admin", methods=['GET', 'POST'])
 def admin():
   return render_template('admin/login.html')
+
+@app.route("/admin/menu", methods=['GET', 'POST'])
+def admin_menu():
+    return render_template('admin/menu.html')
 
 
 if __name__ == "__main__":
