0dc1eb3a847f1eadde1d77651c4a60339c6a0852
Pedro Farias <pfarias29@gmail.com>
2024-08-14 18:12:54 -0300

    feat: comittaço

--- src/app.py
+++ src/app.py
@@ -22,31 +22,29 @@ def logar_aluno():
 
 @app.route("/admin", methods=['GET', 'POST'])
 def admin():
+  if (request.method == 'POST'):
+    login = request.form.get('login')
+    senha = request.form.get('password')
+    if service.logar_admin(login, senha):
+      return render_template('admin/menu.html')
+    else:
+      return render_template('admin/login.html', error='Login ou senha inválidos')
+
   return render_template('admin/login.html')
 
-@app.post("/admin/login")
-def login():
-  login = request.form.get('login')
-  senha = request.form.get('password')
-
-  if service.logar_admin(login, senha):
-    return render_template('admin/menu.html')
-  else:
-    return render_template('admin/login.html', error='Login ou senha inválidos')
-  
 #Tela e rotas do menu do admin
 
 @app.route("/admin/menu", methods=['GET', 'POST'])
 def admin_menu():
-    return render_template('admin/menu.html')
+    if (request.method == 'POST'):
+        cpf = request.form.get('cpf')
+        if service.criar_aluno(cpf):
+            return render_template('admin/menu.html', success='Aluno criado com sucesso')
+        else:
+            return render_template('admin/menu.html', error='Erro ao criar aluno')
 
-@app.post("/admin/criar_aluno")
-def criar_aluno():
-  cpf = request.form.get('cpf')
-  if service.criar_aluno(cpf):
-    return render_template('admin/menu.html', success='Aluno criado com sucesso')
-  else:
-    return render_template('admin/menu.html', error='Erro ao criar aluno')
+
+    return render_template('admin/menu.html')
   
 
 
